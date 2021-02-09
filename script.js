0c7e10f861a006d36ade0e3bcb70489b40c48fc7
Ahiram Cordova <Acordo287@west-mec.local>
2021-02-08 17:15:26 -0700

    added all files to repository

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("McG").addEventListener("click", function() {
+    alert("Are You Sure About That!?");
+});
+
+function myFunction() {
+    alert("NIXON and mcgovern are to be chosen, choose correctly. You don't want the rest of your life to be doomed");
+};
+
+function myFunction() {
+    var person = prompt("Please enter your name", "First Last");
+    if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! Choose Correctly or Else?";
+    }
+};
